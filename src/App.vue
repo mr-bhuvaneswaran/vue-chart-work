24ffa9d89c88c467893fcbdf8ea063e22342d993
bhuvi <bhuvaneswaran@zetwerk.com>
2020-08-23 16:05:07 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <canvas id="myChart" width="1800" height="400"></canvas>
+  </div>
+</template>
+
+<script>
+import Chart from 'chart.js';
+import planetChartData from './chart-data.js';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      planetChartData
+    }
+  },
+  mounted () {
+    this.createChart('myChart', this.planetChartData);
+  },
+  methods: {
+    createChart(chartId, chartData) {
+      const ctx = document.getElementById(chartId);
+      new Chart(ctx, {
+        type: chartData.type,
+        data: chartData.data,
+        options: chartData.options,
+      });
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  max-width: 1800px;
+  max-height: 400px;
+}
+</style>
